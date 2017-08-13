3ad1067ad3070f85bbb8cd2fffff6cd00d9d3641
Lukasz <lukaszandruchow@gmail.com>
2017-08-13 19:45:25 +0100

    trying to export component not a function

--- src/components/presentational/Profile/Profile.js
+++ src/components/presentational/Profile/Profile.js
@@ -1,52 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
-import { ProfileWrapper, ProfileContainer, ProfileNav, ProgressBar, SectionBox, SectionHead, Link, SectionWrap, Description } from './styles.js';
-
-const sections = [
-  { Title: 'About', Desc: 'About you' },
-  { Title: 'Skills', Desc: 'diff skills' },
-  { Title: 'Language skills', Desc: 'My languages & proficiency levels' },
-  { Title: 'Links', Desc: 'My social media & homepage links' },
-  { Title: 'Work Experience', Desc: 'Previous & current positions' },
-  { Title: 'Education', Desc: 'Schools & courses' },
-];
+import { ProfileWrapper, ProfileContainer, ProfileNav, ProgressBar, Link } from './styles.js';
 
 
-export function Profile() {
-  return (
-    <ProfileWrapper>
-      <ProfileContainer>
-        <ProfileNav>
-          <ul>
-            <li><Link to="/">My Profile & CV</Link></li>
-            <li><Link to="/">My Preferences</Link></li>
-          </ul>
-        </ProfileNav>
-        <ProgressBar>
-          <span>Here we will drop avatar and lift it up to cover space in between of 2 tabs</span>
-        </ProgressBar>
+class Profile extends Component {
+  constructor(props) {
+    super(props);
 
-        <ProfileSections sections={sections} />
-      </ProfileContainer>
-    </ProfileWrapper>
-  );
+    this.state = {
+      skills: 'skill',
+      languages: 'language',
+    };
+  }
+
+  render() {
+    return (
+      <ProfileWrapper>
+        <ProfileContainer>
+          <ProfileNav>
+            <ul>
+              <li><Link to="/">My Profile & CV</Link></li>
+              <li><Link to="/">My Preferences</Link></li>
+            </ul>
+          </ProfileNav>
+          <ProgressBar>
+            <span>Here we will drop avatar and lift it up to cover space in between of 2 tabs</span>
+          </ProgressBar>
+        </ProfileContainer>
+      </ProfileWrapper>
+    );
+  }
 }
 
-const ProfileSections = ({ sections }) => {
-  const sectionsJSX = sections.map(section => (
-    <SectionBox>
-      <SectionHead>
-        <strong>{section.Title}</strong>
-        <Description>
-          {section.Desc}
-        </Description>
-      </SectionHead>
-    </SectionBox>
-  ));
-
-  return (
-    <SectionWrap>
-      {sectionsJSX}
-    </SectionWrap>
-  );
-};
+export default Profile;

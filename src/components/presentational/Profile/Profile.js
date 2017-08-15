06190adcbad142ee8785cd7f03693d6965aa9c9c
Lukasz <lukaszandruchow@gmail.com>
2017-08-15 15:03:57 +0100

    edit button shows/hides input field

--- src/components/presentational/Profile/Profile.js
+++ src/components/presentational/Profile/Profile.js
@@ -14,6 +14,7 @@ export class Profile extends Component {
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.onClick = this.onClick.bind(this);
   }
 
   handleSubmit(event) {

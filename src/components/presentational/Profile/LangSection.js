b1f865a98af1fd2127f0aa817b94bc8e93477b12
Lukasz <lukaszandruchow@gmail.com>
2017-08-17 16:28:39 +0100

    adds basic element to new Languages Component

--- src/components/presentational/Profile/LangSection.js
+++ src/components/presentational/Profile/LangSection.js
@@ -1,1 +1,19 @@
 import React, { Component } from 'react';
+import { SectionWrapper } from './styles';
+
+
+export class LangSection extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <SectionWrapper />
+    );
+  }
+}

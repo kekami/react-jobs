36e1790f19051f08f189e14e0a1d7ae7455e5c39
Lukasz <lukaszandruchow@gmail.com>
2017-08-17 16:36:37 +0100

    Adds working Languages Section

--- src/components/presentational/Profile/LangSection.js
+++ src/components/presentational/Profile/LangSection.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { SectionWrapper } from './styles';
+import { SectionWrapper, First2, Edit, Btn, Inputform, I, TagsForm, SkillTag } from './styles';
 
 
 export class LangSection extends Component {
@@ -7,13 +7,72 @@ export class LangSection extends Component {
     super(props);
 
     this.state = {
-
+      show: false,
+      addSkill: '',
+      tags: [],
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.deleteTag = this.deleteTag.bind(this);
+  }
+
+  onClick() {
+    this.setState({ show: !this.state.show });
+  }
+  deleteTag(index) {
+    const tags = this.state.tags.filter((tag, i) => i !== index);
+    this.setState({ tags });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const newTag = {
+      title: this.state.inputValue,
+      isShowing: true,
+    };
+    const tags = this.state.tags.concat(newTag);
+    this.setState({
+      tags,
+      inputValue: '',
+    });
+  }
+
+  addingTags() {
+    return this.state.tags.map((tag, i) => (
+      <SkillTag key={i} onClick={() => this.deleteTag(i)} showing={tag.isShowing}>
+        <Btn>{tag.title} <I className="fa fa-times" aria-hidden="true" /></Btn>
+      </SkillTag>
+    ),
+    );
   }
 
   render() {
     return (
-      <SectionWrapper />
+      <SectionWrapper>
+        <First2>
+          <div>
+            <p><strong>Languages</strong> My languages</p>
+            <Edit onClick={this.onClick}>
+              { this.state.show ?
+                <div><I className="fa fa-floppy-o" aria-hidden="true" /> Save</div>
+                : <div><I className="fa fa-pencil" aria-hidden="true" /> Edit</div> }
+
+            </Edit>
+          </div>
+          <Inputform style={{ display: this.state.show ? 'block' : 'none' }} onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              onChange={e => this.setState({ inputValue: e.target.value })}
+              value={this.state.inputValue}
+              placeholder="Type in a skill and hit Enter"
+            />
+          </Inputform>
+          <TagsForm>
+            {this.addingTags(this.state.addSkill)}
+          </TagsForm>
+        </First2 >
+      </SectionWrapper>
     );
   }
 }

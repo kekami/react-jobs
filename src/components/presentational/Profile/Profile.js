0d5949832c53c799011a1b59f846a4c73425e616
Lukasz <lukaszandruchow@gmail.com>
2017-08-17 16:16:52 +0100

    Adds compartmentalization of sections. 1st Skills.

--- src/components/presentational/Profile/Profile.js
+++ src/components/presentational/Profile/Profile.js
@@ -1,99 +1,23 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
-import { ProfileWrapper, ProfileContainer, ProfileNav, ProgressBar, Link, SectionWrapper, First2, Edit, Btn, Inputform, I, TagsForm, SkillTag } from './styles';
+import { SkillSection } from './SkillSection';
+import { ProfileWrapper, ProfileContainer, ProfileNav, ProgressBar, Link } from './styles';
 
 
-export class Profile extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      show: false,
-      addSkill: '',
-      tags: [],
-    };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.onClick = this.onClick.bind(this);
-    this.deleteTag = this.deleteTag.bind(this);
-  }
-
-
-  onClick() {
-    this.setState({ show: !this.state.show });
-  }
-  deleteTag(index) {
-    const tags = this.state.tags.filter((tag, i) => i !== index);
-    this.setState({ tags });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const newTag = {
-      title: this.state.inputValue,
-      isShowing: true,
-    };
-    const tags = this.state.tags.concat(newTag);
-    this.setState({
-      tags,
-      inputValue: '',
-    });
-  }
-
-  addingTags() {
-    return this.state.tags.map((tag, i) => (
-      <SkillTag key={i} onClick={() => this.deleteTag(i)} showing={tag.isShowing}>
-        <Btn>{tag.title} <I className="fa fa-times" aria-hidden="true" /></Btn>
-      </SkillTag>
-    ),
-    );
-  }
-
-  skill() {
-    return (
-      <SectionWrapper>
-        <First2>
-          <div>
-            <p><strong>Skills</strong> My important skills</p>
-            <Edit onClick={this.onClick}>
-              { this.state.show ?
-                <div><I className="fa fa-floppy-o" aria-hidden="true" /> Save</div>
-                : <div><I className="fa fa-pencil" aria-hidden="true" /> Edit</div> }
-
-            </Edit>
-          </div>
-          <Inputform style={{ display: this.state.show ? 'block' : 'none' }} onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              onChange={e => this.setState({ inputValue: e.target.value })}
-              value={this.state.inputValue}
-              placeholder="Type in a skill and hit Enter"
-            />
-          </Inputform>
-          <TagsForm>
-            {this.addingTags(this.state.addSkill)}
-          </TagsForm>
-        </First2>
-      </SectionWrapper>
-    );
-  }
-
-  render() {
-    return (
-      <ProfileWrapper>
-        <ProfileContainer>
-          <ProfileNav>
-            <ul>
-              <li><Link to="/">My Profile & CV</Link></li>
-              <li><Link to="/">My Preferences</Link></li>
-            </ul>
-          </ProfileNav>
-          <ProgressBar>
-            <span>Here we will drop avatar and lift it up to cover space in between of 2 tabs</span>
-          </ProgressBar>
-          {this.skill()}
-        </ProfileContainer>
-      </ProfileWrapper>
-    );
-  }
+export function Profile() {
+  return (
+    <ProfileWrapper>
+      <ProfileContainer>
+        <ProfileNav>
+          <ul>
+            <li><Link to="/">My Profile & CV</Link></li>
+            <li><Link to="/">My Preferences</Link></li>
+          </ul>
+        </ProfileNav>
+        <ProgressBar>
+          <span>Here we will drop avatar and lift it up to cover space in between of 2 tabs</span>
+        </ProgressBar>
+        <SkillSection />
+      </ProfileContainer>
+    </ProfileWrapper>
+  );
 }
---
+++ src/components/presentational/Profile/SkillSection.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+import { SectionWrapper, First2, Edit, Btn, Inputform, I, TagsForm, SkillTag } from './styles';
+
+
+export class SkillSection extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: false,
+      addSkill: '',
+      tags: [],
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.deleteTag = this.deleteTag.bind(this);
+  }
+
+
+  onClick() {
+    this.setState({ show: !this.state.show });
+  }
+  deleteTag(index) {
+    const tags = this.state.tags.filter((tag, i) => i !== index);
+    this.setState({ tags });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const newTag = {
+      title: this.state.inputValue,
+      isShowing: true,
+    };
+    const tags = this.state.tags.concat(newTag);
+    this.setState({
+      tags,
+      inputValue: '',
+    });
+  }
+
+  addingTags() {
+    return this.state.tags.map((tag, i) => (
+      <SkillTag key={i} onClick={() => this.deleteTag(i)} showing={tag.isShowing}>
+        <Btn>{tag.title} <I className="fa fa-times" aria-hidden="true" /></Btn>
+      </SkillTag>
+    ),
+    );
+  }
+
+  render() {
+    return (
+      <SectionWrapper>
+        <First2>
+          <div>
+            <p><strong>Skills</strong> My important skills</p>
+            <Edit onClick={this.onClick}>
+              { this.state.show ?
+                <div><I className="fa fa-floppy-o" aria-hidden="true" /> Save</div>
+                : <div><I className="fa fa-pencil" aria-hidden="true" /> Edit</div> }
+
+            </Edit>
+          </div>
+          <Inputform style={{ display: this.state.show ? 'block' : 'none' }} onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              onChange={e => this.setState({ inputValue: e.target.value })}
+              value={this.state.inputValue}
+              placeholder="Type in a skill and hit Enter"
+            />
+          </Inputform>
+          <TagsForm>
+            {this.addingTags(this.state.addSkill)}
+          </TagsForm>
+        </First2>
+      </SectionWrapper>
+    );
+  }
+}

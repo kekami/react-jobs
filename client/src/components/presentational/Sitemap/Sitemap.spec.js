f1dc0cf6a8cef3b758f19ddf2f5a88019f30efbc
Lukasz <lukaszandruchow@gmail.com>
2017-09-13 14:28:04 +0100

    Completing basic test suite for Sitemap component

--- client/src/components/presentational/Sitemap/Sitemap.spec.js
+++ client/src/components/presentational/Sitemap/Sitemap.spec.js
@@ -2,15 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
 import { mount, shallow } from 'enzyme';
-import { Sitemap, SitemapWrapper } from './Sitemap';
+import toJson from 'enzyme-to-json';
+import { Sitemap } from './Sitemap';
 
 
 describe('Sitemap Component', () => {
-  it('Sitemap renders correctly', () => {
+  it('Sitemap (and children) renders correctly', () => {
     const div = document.createElement('div');
     ReactDOM.render(
       <MemoryRouter>
         <Sitemap />
       </MemoryRouter>, div);
+  });
+
+  it('Sitemap renders correclty/ shallow test render', () => {
+    const wrapper = shallow(<Sitemap />);
+  });
+
+  it('Sitemap matches its shallow snapshot', () => {
+    const wrapper = shallow(<Sitemap />);
+    expect(toJson(wrapper)).toMatchSnapshot()
   });
 });

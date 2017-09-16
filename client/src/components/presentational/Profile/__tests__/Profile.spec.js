50e1743d1e05e0f7570704cb852a359f956d6c71
Lukasz <lukaszandruchow@gmail.com>
2017-09-16 21:30:09 +0100

    Adds basic tests for Profile section

--- client/src/components/presentational/Profile/__tests__/Profile.spec.js
+++ client/src/components/presentational/Profile/__tests__/Profile.spec.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import { MemoryRouter } from 'react-router-dom';
 import { Profile } from '../Profile';
 
-describe('Profile component', () => {
+// Tests for Profile Section
+/*
+1. basic render
+2. snapshot
+*/
+
+describe('Profile component - basic test', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(
       <MemoryRouter>
         <Profile />
       </MemoryRouter>, div);
+  });
+
+  it('Profile component matches its shallow snapshot', () => {
+    const wrapper = shallow(<Profile />);
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
